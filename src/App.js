ccee1fbd4893bd1b09bb994a86f7091144674ef2
czhang0141 <66802578+czhang0141@users.noreply.github.com>
2022-06-24 15:41:01 -0500

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Image from './Images/Cristina.jpg'
+import About from './Portfolio/About';
+// import Resume from './Portfolio/Resume';
+import Projects from './Portfolio/Projects';
+import Contact from './Portfolio/Contact';
+import Navbar from './Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="">
-
-    </div>
+      <div className="App">
+        <img src={Image} alt="Cristina Zhang" />
+        <Router>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<About />}>
+              <Route path="projects" element={<Projects />} />
+              <Route path="contact" element={<Contact />} />
+            </Route>
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
